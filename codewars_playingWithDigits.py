9ce76b7243d66de87e9116486adb78534b314af4
icodetohack <76577918+icodetohack@users.noreply.github.com>
2021-06-03 09:56:44 +0530

    Update codewars_playingWithDigits.py

--- codewars_playingWithDigits.py
+++ codewars_playingWithDigits.py
@@ -1,4 +1,4 @@
-#question
+### Descriptio:
 Some numbers have funny properties. For example:
 
 89 --> 8¹ + 9² = 89 * 1
@@ -18,12 +18,13 @@ If it is the case we will return k, if not return -1.
 
 Note: n and p will always be given as strictly positive integers.
 
+### Example:
 dig_pow(89, 1) should return 1 since 8¹ + 9² = 89 = 89 * 1
 dig_pow(92, 1) should return -1 since there is no k such as 9¹ + 2² equals 92 * k
 dig_pow(695, 2) should return 2 since 6² + 9³ + 5⁴= 1390 = 695 * 2
 dig_pow(46288, 3) should return 51 since 4³ + 6⁴+ 2⁵ + 8⁶ + 8⁷ = 2360688 = 46288 * 51
 
-#solution
+## Solution : 
 
 def dig_pow(n, p):
     import math as m
